96646e641098df28708af760b8406f4007a56090
Kamil Czubaszek <kamil.czubaszek.1994@gmail.com>
2023-04-17 16:41:25 +0200

    first commit

---
+++ src/main/java/com/empik/githubadapter/GithubAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.empik.githubadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubAdapterApplication.class, args);
+	}
+
+}
